b2bbb5ba53dcf09581da913ecd5ae73dd0b1dad6
SaintsaminSalem <ekariasalem1@gmail.com>
2023-07-15 09:27:17 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,7 +7,6 @@
 int main(void)
 {
 	int num1, num2;
-	
 	for (num1 = 0; num1 <= 98; num1++)
 	{
 		for (num2 = num1 + 1; num2 <= 99; num2++)
@@ -17,7 +16,6 @@ int main(void)
 			putchar(' ');
 			putchar(num2 / 10 + '0');
 			putchar(num2 % 10 + '0');
-			
 			if (num1 != 98 || num2 != 99)
 			{
 				putchar(',');
@@ -25,9 +23,6 @@ int main(void)
 			}
 		}
 	}
-	
 	putchar('\n');
-	
 	return 0;
 }
-
