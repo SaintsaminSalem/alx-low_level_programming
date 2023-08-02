57df6583d3fde742e39731435e8109b97e113964
SaintsaminSalem <ekariasalem1@gmail.com>
2023-08-02 06:30:44 -0700

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 5-main.c 5-sqrt_recursion.c -o 5-sqrt

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
-
 /**
- * _sqrt_recurssion - finds natural square root of a number.
+ * _sqrt_recursion - finds natural square root
  * @n: int
  * @val: square root
  * Return: int
@@ -12,14 +11,12 @@ int _sqrt_recursion(int n)
 {
 return (square(n, 1));
 }
-
 /**
  * square - find square root
- * @n int to find square root
+ * @n: int to find square root
  * @val: square root
  * Return: int
  */
-
 int square(int n, int val)
 {
 if (val * val == n)
