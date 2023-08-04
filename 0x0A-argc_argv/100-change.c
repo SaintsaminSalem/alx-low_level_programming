3b59a26bfdc14955ba5471db9966794bb87284eb
SaintsaminSalem <ekariasalem1@gmail.com>
2023-08-04 09:11:13 -0700

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 100-change.c -o change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -15,39 +15,41 @@ int main(int argc, char *argv[])
 	int cents, coins = 0;
 
 	if (argc != 2)
+
 	{
-		 printf("Error\n");
-		 return (1);
+		printf("Error\n");
+		return (1);
 	}
-
-	cents = atoi(argv[1]);
-
-	while (cents > 0)
+	else
 	{
-		coins++;
-		if ((cents - 25) >= 0)
-		{
-			cents -= 25;
-			continue;
-		}
-		if ((cents - 10) >= 0)
-		{
-			cents -= 10;
-			continue;
-		}
-		if ((cents - 5) >= 0)
-		{
-			cents -= 5;
-			continue;
-		}
-		if ((cents - 2) >= 0)
-		{
-			cents -= 2;
-			continue;
-		}
-		cents--;
-	}
-	printf("%d\n", coins);
+		cents = atoi(argv[1]);
 
+		while (cents > 0)
+		{
+			coins++;
+			if ((cents - 25) >= 0)
+			{
+				cents -= 25;
+				continue;
+			}
+			else if ((cents - 10) >= 0)
+			{
+				cents -= 10;
+				continue;
+			}
+			else if ((cents - 5) >= 0)
+			{
+				cents -= 5;
+				continue;
+			}
+			else if ((cents - 2) >= 0)
+			{
+				cents -= 2;
+				continue;
+			}
+			cents--;
+		}
+		printf("%d\n", coins);
+	}
 	return (0);
 }
