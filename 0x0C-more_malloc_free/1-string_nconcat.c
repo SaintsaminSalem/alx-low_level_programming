f458674f47784bb31713fc4b4a15c4daa73bee79
SaintsaminSalem <ekariasalem1@gmail.com>
2023-08-11 05:04:28 -0700

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 1-main.c 1-string_nconcat.c -o 1-string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -28,7 +28,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		k++;
 
 	if (n >= k)
-		l = i + n;
+		l = i + k;
 	else
 		l = i + n;
 
@@ -40,7 +40,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	while (j < l)
 	{
 		if (j <= i)
-			str[j] = s1[i];
+			str[j] = s1[j];
 
 		if (j >= i)
 		{
